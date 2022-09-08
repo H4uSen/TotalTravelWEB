9204e5d0c59db11413c81daa6dca1c6e11f51ee7
Claudia1234567890 <109091065+Claudia1234567890@users.noreply.github.com>
2022-09-08 11:12:54 -0600

    Tabla y botones de países

--- Prototipos/TotalTravelPrototipo/TotalTravelPrototipo/wwwroot/js/site.js
+++ Prototipos/TotalTravelPrototipo/TotalTravelPrototipo/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+﻿$(document).ready(function () {
+    $('#example').DataTable({
+        language: {
+            "url": "https://cdn.datatables.net/plug-ins/1.10.21/i18n/Spanish.json"
+        }
+    });
+});
+
+// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
