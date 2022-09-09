f886f258a611112df06aee0c0bcca4c93dcd7738
Jahdiel17 <77807529+Jahdiel17@users.noreply.github.com>
2022-09-09 03:37:31 -0600

    Pantalla de Transportes SIN DETALLES

---
+++ Prototipos/TotalTravelPrototipo/TotalTravelPrototipo/Controllers/TransporteController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TotalTravelPrototipo.Controllers
+{
+    public class TransporteController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Details()
+        {
+            return View();
+        }
+
+       
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        
+    }
+}
