1392a468d700fed0351e3ddc2006ded98a7f66d0
BU3S0 <109097992+BU3S0@users.noreply.github.com>
2022-09-12 06:18:42 -0600

    Crud partners

---
+++ Prototipos/TotalTravelPrototipo/TotalTravelPrototipo/Controllers/PartnersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TotalTravelPrototipo.Controllers
+{
+    public class PartnersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
