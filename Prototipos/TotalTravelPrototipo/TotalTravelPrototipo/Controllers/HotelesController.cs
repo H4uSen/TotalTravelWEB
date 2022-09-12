b79fc269e6c39df1dced8334fe27a118c68512d0
Kiritsugu-sama <109097530+Kiritsugu-sama@users.noreply.github.com>
2022-09-12 09:45:35 -0600

    CRUD HOTELES - Alvaro Reyes

---
+++ Prototipos/TotalTravelPrototipo/TotalTravelPrototipo/Controllers/HotelesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TotalTravelPrototipo.Controllers
+{
+    public class HotelesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+    }
+}
