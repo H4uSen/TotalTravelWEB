9c82c12f419b013c273108756f949bc4fd4ecd61
Rodro08 <roodrigoaraque1002@gmail.com>
2022-10-10 23:13:21 -0600

    Intento de Crud Detalles transportes

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Models/TransportDetailsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_WEB.Models
+{
+    public class TransportDetailsListViewModel
+    {
+        public int ID { get; set; }
+        public string Transporte { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Hora_Salida { get; set; }
+        public string Hora_Llegada { get; set; }
+        public int Capacidad { get; set; }
+        public decimal? Precio { get; set; }
+        public string Matricula { get; set; }
+        public string Usuario_Creacion { get; set; }
+        public DateTime? Fecha_Creacion { get; set; }
+        public string Usuario_Modifica { get; set; }
+        public DateTime? Fecha_Modifica { get; set; }
+        public bool? Estado { get; set; }
+
+    }
+
+    public class TransportDetailsViewModel
+    {
+        public int Tprt_ID { get; set; }
+        public int HoTr_ID { get; set; }
+        public int Detr_Capacidad { get; set; }
+        public decimal? DeTr_Precio { get; set; }
+        public string DeTr_Matricula { get; set; }
+        public string DeTr_UsuarioCreacion { get; set; }
+        public string DeTr_UsuarioModifica { get; set; }
+
+    }
+}
---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Services/TransportService.cs
@@ -0,0 +1,51 @@
+﻿using AHM_TOTAL_TRAVEL_WEB.Models;
+using AHM_TOTAL_TRAVEL_WEB.WebAPI;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_WEB.Services
+{
+    public class TransportService 
+    {
+        private readonly API _api;
+
+        public TransportService(API api)
+        {
+            _api = api;
+        }
+
+        #region DetallesTransportes
+        public async Task<ServiceResult> TransportDetailsList(string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Get<IEnumerable<TransportDetailsListViewModel>, IEnumerable<TransportDetailsListViewModel>>(req => {
+                    req.Path = $"/API/DetailsTransport/List";
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+        #endregion
+
+    }
+}
