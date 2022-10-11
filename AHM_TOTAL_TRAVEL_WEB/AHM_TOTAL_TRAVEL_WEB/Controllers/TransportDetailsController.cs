cb6d511bc90715157a9b44352b65b1b0fd18f9fd
Rodro08 <roodrigoaraque1002@gmail.com>
2022-10-11 00:59:19 -0600

    Intento de Crud de Detalles Transporte

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/TransportDetailsController.cs
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/TransportDetailsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AHM_TOTAL_TRAVEL_WEB.Models;
+using AHM_TOTAL_TRAVEL_WEB.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +11,51 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
 {
     public class TransportDetailsController : Controller
     {
-        public IActionResult Index()
+        private readonly TransportService _transportService;
+
+        public TransportDetailsController(TransportService transportService)
         {
+            _transportService = transportService;
+        }
+
+        //[HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = new List<TransportDetailsListViewModel>();
+            var list = await _transportService.TransportDetailsList(model);
+            return View(list.Data);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            var model = new List<TransportDetailsViewModel>();
+
+            //IEnumerable<RestaurantViewModel> model_restaurant = null;
+            //var restaurant = await _restaurantServices.RestaurantCreate(model_restaurant);
+            //IEnumerable<PaisesListViewModel> data_Pais = (IEnumerable<PaisesListViewModel>)pais.Data;
+            //ViewBag.Pais_ID = new SelectList(data_Pais, "ID", "Descripcion");
+
+
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TransportDetailsViewModel transportdetails)
+        {
+
+            if (ModelState.IsValid)
+            {
+                string token = HttpContext.User.FindFirst("Token").Value;
+                transportdetails.DeTr_UsuarioCreacion = HttpContext.User.FindFirst("User_Id").Value;
+                var list = await _transportService.TransportDetailsCreate(transportdetails, token);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
+
         }
     }
 }
