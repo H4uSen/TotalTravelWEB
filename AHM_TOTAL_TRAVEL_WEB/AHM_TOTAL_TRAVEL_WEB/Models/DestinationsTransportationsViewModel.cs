4b5fef27d94e3d4e1da9a17cac30c58bda4f678e
May03 <105520512+MayCruz03@users.noreply.github.com>
2022-10-13 21:50:27 -0600

    Avance crud Destinos Transportes

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Models/DestinationsTransportationsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_WEB.Models
+{
+    public class DestinationsTransportationsViewModel
+    {
+        public int DsTr_ID { get; set; }
+        public int? DsTr_CiudadSalida { get; set; }
+        public int? DsTr_CiudadDestino { get; set; }
+        public int? DsTr_UsuarioCreacion { get; set; }
+        public DateTime? DsTr_FechaCreacion { get; set; }
+        public int? DsTr_UsuarioModifica { get; set; }
+        public DateTime? DsTr_FechaModifica { get; set; }
+        public bool? DsTr_Estado { get; set; }
+    }
+
+    public class DestinationsTransportationsListViewModel
+    {
+        public int ID { get; set; }
+        public string CiudadSalida { get; set; }
+        public int CiudadSalidaID { get; set; }
+        public string CiudadDestino { get; set; }
+        public int CiudadDestinoID { get; set; }
+        public int? UsuarioCreacion { get; set; }
+        public string UsuarioCreacionID { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public int? UsuarioModifica { get; set; }
+        public string UsuarioModificaID { get; set; }
+        public DateTime? FechaModifica { get; set; }
+        public bool? Estado { get; set; }
+    }
+}
---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/DestinationsTransportationsJs/Create.js
@@ -0,0 +1,34 @@
+﻿$("#errorDiv").hide();
+
+$('.ui.dropdown').dropdown();
+
+$("#createDestinationsTransportations").click(() => {
+    $("#modalCreate").modal('show');
+});
+
+$("#closeActivity").click(() => {
+    $("#modalCreate").modal('hide');
+});
+
+$("#sendActivity").click(() => {
+    validar();
+})
+
+function validar() {
+    if ($("#Actividad").val() == 0) {
+        $("#labelvalidatorActividad").html("Ingrese una Descripcion");
+    }
+    else {
+        $("#labelvalidatorActividad").html(" ");
+    }
+    if ($("#tipoActividad").val() == 0) {
+        $("#labelvalidatorTipoActividad").html("Seleccione el tipo de la Actividad");
+    }
+    else {
+        $("#labelvalidatorTipoActividad").html(" ");
+    }
+    if ($("#Actividad").val() != 0 && $("#tipoActividad").val() != 0) {
+        $("#createDestinationsTransportationsForm").submit();
+    }
+
+}
