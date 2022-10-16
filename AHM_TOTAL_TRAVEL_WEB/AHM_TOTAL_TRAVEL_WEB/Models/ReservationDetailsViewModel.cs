d0a5458abe43d0be1027c2d5b1c1e2705da6f6ce
May03 <105520512+MayCruz03@users.noreply.github.com>
2022-10-16 17:44:26 -0600

    Avance xd

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Models/ReservationDetailsViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_WEB.Models
+{
+    public class ReservationDetailsViewModel
+    {
+        public int ReDe_ID { get; set; }
+        public int? Habi_ID { get; set; }
+        public int? ReHo_ID { get; set; }
+        public int? ReDe_UsuarioCreacion { get; set; }
+        public DateTime? ReDe_FechaCreacion { get; set; }
+        public int? ReDe_UsuarioModifica { get; set; }
+        public DateTime? ReDe_FechaModifica { get; set; }
+        public bool? ReDe_Estado { get; set; }
+    }
+
+    public class ReservationDetailsListViewModel
+    {
+        public int ID { get; set; }
+        public string Nombre_Habitacion { get; set; }
+        public string Descripcion_Habitacion { get; set; }
+        public string Categoria_Habitacion { get; set; }
+        public string Hotel { get; set; }
+        public decimal? Precio_Habitacion { get; set; }
+        public DateTime? Fecha_Entrada { get; set; }
+        public DateTime? Fecha_Salida { get; set; }
+        public decimal? Precio_ReservacionHotel { get; set; }
+        public string Usuario_Creacion { get; set; }
+        public DateTime? Fecha_Creacion { get; set; }
+        public string Usuario_Modifica { get; set; }
+        public DateTime? Fecha_Modifica { get; set; }
+        public bool? Estado { get; set; }
+    }
+}
--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Services/ReservationService.cs
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Services/ReservationService.cs
@@ -226,5 +226,455 @@ namespace AHM_TOTAL_TRAVEL_WEB.Services
 
         }
         #endregion
+
+        #region ReservacionDetalles
+
+        public async Task<ServiceResult> ReservationDetailsList(string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Get<IEnumerable<ReservationDetailsListViewModel>, IEnumerable<ReservationDetailsListViewModel>>(req => {
+                    req.Path = $"/API/ReservationDetails/List";
+
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+
+        public async Task<ServiceResult> ReservationDetailsCreate(ReservationDetailsViewModel reservaciondetalle, string token)
+        {
+            var Result = new ServiceResult();
+            try
+            {
+                var response = await _api.Post<ReservationDetailsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationDetails/Insert";
+                    req.Content = reservaciondetalle;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+
+        }
+
+        public async Task<ServiceResult> ReservationDetailsUpdate(ReservationDetailsViewModel reservaciondetalle, string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Put<ReservationDetailsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationDetails/Update?id=" + reservaciondetalle.ReDe_ID;
+                    req.Content = reservaciondetalle;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> ReservationDetailsFind(string id, string token)
+        {
+            var Result = new ServiceResult();
+            var cuenta = new ReservationDetailsListViewModel();
+
+            try
+            {
+                var response = await _api.Get<ReservationDetailsListViewModel, ReservationDetailsListViewModel>(req => {
+                    req.Path = $"/API/ReservationDetails/Find?id=" + id;
+                    req.Content = cuenta;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> ReservationDetailsDelete(ReservationDetailsViewModel reservaciondetalle, int id, string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Delete<ReservationDetailsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationDetails/Delete?id=" + id + "&mod=" + reservaciondetalle.ReDe_UsuarioModifica;
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region ReservacionHoteles
+
+        public async Task<ServiceResult> ReservationHotelsList(string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Get<IEnumerable<ReservationHotelsListViewModel>, IEnumerable<ReservationHotelsListViewModel>>(req => {
+                    req.Path = $"/API/ReservationHotels/List";
+
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+
+        public async Task<ServiceResult> ReservationHotelsCreate(ReservationHotelsViewModel reservacionhotel, string token)
+        {
+            var Result = new ServiceResult();
+            try
+            {
+                var response = await _api.Post<ReservationHotelsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationHotels/Insert";
+                    req.Content = reservacionhotel;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+
+        }
+
+        public async Task<ServiceResult> ReservationHotelsUpdate(ReservationHotelsViewModel reservacionhotel, string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Put<ReservationHotelsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationHotels/Update?id=" + reservacionhotel.ReHo_ID;
+                    req.Content = reservacionhotel;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> ReservationHotelsFind(string id, string token)
+        {
+            var Result = new ServiceResult();
+            var cuenta = new ReservationHotelsListViewModel();
+
+            try
+            {
+                var response = await _api.Get<ReservationHotelsListViewModel, ReservationHotelsListViewModel>(req => {
+                    req.Path = $"/API/ReservationHotels/Find?id=" + id;
+                    req.Content = cuenta;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> ReservationHotelsDelete(ReservationHotelsViewModel reservacionhotel, int id, string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Delete<ReservationHotelsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationHotels/Delete?id=" + id + "&mod=" + reservacionhotel.ReHo_UsuarioModifica;
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region ReservacionActividadesHoteles
+
+        public async Task<ServiceResult> ReservationActivitiesHotelsList(string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Get<IEnumerable<ReservationActivitiesHotelsListViewModel>, IEnumerable<ReservationActivitiesHotelsListViewModel>>(req => {
+                    req.Path = $"/API/ReservationActivitiesHotels/List";
+
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+
+        public async Task<ServiceResult> ReservationActivitiesHotelsCreate(ReservationActivitiesHotelsViewModel reservacionacthotel, string token)
+        {
+            var Result = new ServiceResult();
+            try
+            {
+                var response = await _api.Post<ReservationActivitiesHotelsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationActivitiesHotels/Insert";
+                    req.Content = reservacionacthotel;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+
+        }
+
+        public async Task<ServiceResult> ReservationActivitiesHotelsUpdate(ReservationActivitiesHotelsViewModel reservacionacthotel, string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Put<ReservationActivitiesHotelsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationActivitiesHotels/Update?id=" + reservacionacthotel.ReAH_ID;
+                    req.Content = reservacionacthotel;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> ReservationActivitiesHotelsFind(string id, string token)
+        {
+            var Result = new ServiceResult();
+            var cuenta = new ReservationActivitiesHotelsListViewModel();
+
+            try
+            {
+                var response = await _api.Get<ReservationActivitiesHotelsListViewModel, ReservationActivitiesHotelsListViewModel>(req => {
+                    req.Path = $"/API/ReservationActivitiesHotels/Find?id=" + id;
+                    req.Content = cuenta;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        public async Task<ServiceResult> ReservationActivitiesHotelsDelete(ReservationActivitiesHotelsViewModel reservacionacthotel, int id, string token)
+        {
+            var Result = new ServiceResult();
+
+            try
+            {
+                var response = await _api.Delete<ReservationActivitiesHotelsViewModel, RequestStatus>(req =>
+                {
+                    req.Path = $"/API/ReservationActivitiesHotels/Delete?id=" + id + "&mod=" + reservacionacthotel.ReAH_UsuarioModifica;
+                    req.Content = null;
+                },
+                token
+                );
+                if (!response.Success)
+                {
+                    return Result.FromApi(response);
+                }
+                else
+                {
+                    return Result.Ok(response.Data);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Result.Error(Helpers.GetMessage(ex));
+                throw;
+            }
+        }
+
+        #endregion
     }
 }
