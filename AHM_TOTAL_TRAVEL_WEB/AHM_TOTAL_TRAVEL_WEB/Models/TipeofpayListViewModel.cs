ca02badd3e710538f50c3bb9f7f5b886e970886f
Choomlee <109097735+Choomlee@users.noreply.github.com>
2022-10-17 08:43:22 -0600

    todo es culpa de angel

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Models/TipeofpayListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_WEB.Models
+{
+    public class TipeofpayListViewModel
+    {
+        public int ID { get; set; }
+        public string? Descripcion { get; set; }
+        public int? UsuarioCreacionID { get; set; }
+        public string UsuarioCreacion { get; set; }
+        public DateTime? FechaCreacion { get; set; }
+        public int? UsuarioModificaID { get; set; }
+        public string UsuarioModifica { get; set; }
+        public DateTime? FechaModifica { get; set; }
+        public bool? Estado { get; set; }
+       
+    }
+}
+
