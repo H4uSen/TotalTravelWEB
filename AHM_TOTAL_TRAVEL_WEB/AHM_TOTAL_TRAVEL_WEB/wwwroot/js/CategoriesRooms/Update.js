9201f60589b9e20a16f1fa5f56b89cddd6a60388
Choomlee <109097735+Choomlee@users.noreply.github.com>
2022-10-17 09:38:17 -0600

    T.P.A

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/CategoriesRooms/Update.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    $("#CaHa_ID").hide();
+
+});
+
+function Updatecategory(id) {
+    var response = ajaxRequest("https://totaltravel.somee.com/API/Categoriesrooms/Find?Id=" + id);
+    if (response.code == 200) {
+        $('#CaHa_ID').val(id);
+        $('#CaHa_DescripcionUpdate').val(response.data.descripcion);
+        $("#modalUpdate").modal('show');
+
+    }
+}
+
+$("#closeEditCategoryrooms").click(() => {
+    $("#modalUpdate").modal('hide');
+});
+
+
+$("#sendEditCategoryrooms").click(() => {
+
+
+
+    validateArrayForm = [
+        { validateMessage: "Ingrese un tipo de pago .", Jqueryinput: $("#CaHa_DescripcionUpdate") },
+    ];
+
+    const ValidateFormStatus = ValidateForm(validateArrayForm);
+    if (ValidateFormStatus) {
+        $('#updateCategoryroomsForm').submit()
+    }
+
+});
