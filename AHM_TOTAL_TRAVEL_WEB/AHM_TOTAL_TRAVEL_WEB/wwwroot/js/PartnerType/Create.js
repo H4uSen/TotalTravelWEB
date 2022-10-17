029d83043f34002237086417fb9c463d9667cd09
Rodro08 <roodrigoaraque1002@gmail.com>
2022-10-16 19:59:14 -0600

    Modal Create Funcional Tipo Partner

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/PartnerType/Create.js
@@ -0,0 +1,22 @@
+﻿$('.ui.dropdown').dropdown();
+
+$("#createTipoPartner").click(() => {
+    $("#modalCreate").modal('show');
+});
+
+$("#closeTipoPartner").click(() => {
+    $("#modalCreate").modal('hide');
+});
+
+$("#sendTipoPartner").click(() => {
+
+    const ValidateArray = [
+        { validateMessage: "Ingrese una descripcion de tipo de socio", Jqueryinput: $("#TiPar_Descripcion") },
+        { validateMessage: "Seleccione un Rol para el socio", Jqueryinput: $("#Rol_ID") }
+    ];
+    const userValidate = ValidateForm(ValidateArray);
+    if (userValidate) {
+        $("#createTipoPartnerForm").submit();
+    }
+
+});
