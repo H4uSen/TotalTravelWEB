212d1af9fb2e781197c64bd70cc3e6e4d9efb9d6
Andres Leiva <andresleivaamaya@gmail.com>
2022-10-17 10:37:34 -0600

    Cambios en el model y controllador

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/RegistrationPaymentsController.cs
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/RegistrationPaymentsController.cs
@@ -11,31 +11,34 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
     public class RegistrationPaymentsController : Controller
     {
         private readonly SaleServices _saleServices;
-        private readonly ReservationService _reservationServices;
+        private readonly ReservationService _reservationServices; 
 
         public RegistrationPaymentsController(SaleServices saleServices, ReservationService reservationServices)
         {
             _saleServices = saleServices;
             _reservationServices = reservationServices;
         }
-
-        //[HttpGet]
-        //public async Task<IActionResult> Index()
-        //{
-        //    var model = new List<Object>();
-        //    string token = HttpContext.Session.("Token");
-
-        //    PaymentRecordListViewModel payment = ((PaymentRecordListViewModel)(await _saleServices.PaymentRecordsList()).Data);
-        //    ReservationListViewModel reservation = ((ReservationListViewModel)(await _reservationServices.ReservationFind(payment.Id_Reservacion, );
-        //    return View(list.Data);
-        //}
         
-        //[HttpGet]
-        //public async Task<IActionResult> Details(string id)
-        //{
-        //    string token = HttpContext.User.FindFirst("Token").Value;
-        //    PaymentRecordViewModel record = (PaymentRecordViewModel)(await _saleServices.PaymentRecordsFind(id, token)).Data;
-        //    return View(record);
-        //}
+
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var model = new List<Object>();
+            string token = HttpContext.User.FindFirst("User_Id").Value;
+
+            List<PaymentRecordListViewModel> payments = ((List<PaymentRecordListViewModel>)(await _saleServices.PaymentRecordsList()).Data);
+            return View(payments);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(string id)
+        {
+            var model = new List<PaymentRecordViewModel>();
+            var list = await _saleServices.PaymentRecordsList();
+            string token = HttpContext.User.FindFirst("Token").Value;
+            PaymentRecordViewModel record = (PaymentRecordViewModel)(await _saleServices.PaymentRecordsFind(id, token)).Data;
+            return View(record);
+        }
     }
 }
