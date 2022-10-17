b1a62cacdc78bc5327bc1623d6d35aaab171e5a0
Werundy <angeljmacea@gmail.com>
2022-10-17 01:01:30 -0600

    Address CRUD, completo. Menus Edit 90% uwu

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/MenusController.cs
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/MenusController.cs
@@ -22,8 +22,7 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
         [HttpGet]
         public async Task<IActionResult> Index()
         {
-            var model = new List<MenusListViewModel>();
-            var list = await _restaurantServices.MenusList(model);
+            var list = await _restaurantServices.MenusList();
             return View(list.Data);
         }
 
@@ -35,13 +34,11 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
             ViewData["Token"] = HttpContext.User.FindFirst("Token").Value;
             ViewData["UserID"] = HttpContext.User.FindFirst("User_Id").Value;
 
-            IEnumerable<RestaurantListViewModel> model_Restaurant= null;
             string token = HttpContext.User.FindFirst("Token").Value;
             var rest = await _restaurantServices.RestaurantsList(token);
             IEnumerable<RestaurantListViewModel> data_Rest = (IEnumerable<RestaurantListViewModel>)rest.Data;
             ViewBag.Rest_ID = new SelectList(data_Rest, "ID", "Restaurante");
 
-            IEnumerable<TypeMenusListViewModel> model_TipoMenus = null;
             var typeMenus = await _restaurantServices.TypeMenusList();
             IEnumerable<TypeMenusListViewModel> data_TypeMenus = (IEnumerable<TypeMenusListViewModel>)typeMenus.Data;
             ViewBag.TiMe_ID = new SelectList(data_TypeMenus, "ID", "descripcion");
@@ -73,6 +70,40 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
             string token = HttpContext.User.FindFirst("Token").Value;
             var detalle = (MenusListViewModel)(await _restaurantServices.MenusFind(id, token)).Data;
             return View(detalle);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            ViewData["Token"] = HttpContext.User.FindFirst("Token").Value;
+            ViewData["UserID"] = HttpContext.User.FindFirst("User_Id").Value;
+
+            string token = HttpContext.User.FindFirst("Token").Value;
+            var rest = await _restaurantServices.RestaurantsList(token);
+            IEnumerable<RestaurantListViewModel> data_Rest = (IEnumerable<RestaurantListViewModel>)rest.Data;
+            ViewBag.Rest_ID = new SelectList(data_Rest, "ID", "Restaurante");
+
+            var typeMenus = await _restaurantServices.TypeMenusList();
+            IEnumerable<TypeMenusListViewModel> data_TypeMenus = (IEnumerable<TypeMenusListViewModel>)typeMenus.Data;
+            ViewBag.TiMe_ID = new SelectList(data_TypeMenus, "ID", "descripcion");
+
+            var item = new MenusListViewModel();
+            var list = await _restaurantServices.MenusList();
+            IEnumerable<MenusListViewModel> data = (IEnumerable<MenusListViewModel>)list.Data;
+            var element = data.Where(x => x.ID == id).ToList()[0];
+
+            item.Menu = element.Menu;
+            item.Descripcion = element.Descripcion;
+            string precio = Convert.ToString(element.Precio);
+            item.Precio = Convert.ToInt32(precio.Remove(precio.Length - 5, 5));
+
+            ViewData["MenusFolder"] = $"Restaurants/Restaurant-{element.ID_Restaurante}/Food";
+            ViewData["RestaurantID"] = element.ID_Restaurante;
+            ViewData["TipoMenuID"] = element.ID_TipoMenu;
+            ViewData["MenuID"] = id;
+
+
+            return View(item);
         }
     }
 }
