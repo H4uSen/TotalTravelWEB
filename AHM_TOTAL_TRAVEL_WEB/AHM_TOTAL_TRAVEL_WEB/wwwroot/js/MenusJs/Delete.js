f2ba66dff0337c58d956711666249749af01f92c
Werundy <angeljmacea@gmail.com>
2022-10-17 23:04:42 -0600

    Restaurantes, TypeMenus y Menus corregidos

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/MenusJs/Delete.js
@@ -0,0 +1,32 @@
+﻿// ----------------------------------- EVENTS ------------------------------------
+$("#txtSearch").keyup(function () {
+    _this = this;
+    $.each($("#grdMenus tbody tr"), function () {
+        if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+            $(this).hide();
+        }
+        else {
+            $(this).show();
+        }
+    });
+
+    $("#grdMenus").paginationTdA({
+        elemPerPage: 10
+    });
+});
+
+$("#grdMenus").paginationTdA({
+    elemPerPage: 10
+});
+
+
+function DeleteMenus(id) {
+    const capsula1 = () => {
+        var response = ajaxRequest("Menus/Delete?id=" + id, null, "POST");
+        if (response > 0) {
+            window.location.href = '/Menus?success=true';
+        }
+    };
+    sweetAlertconfirm("¿Seguro de eliminar este registro?", "Este registro se borrara permanentemente.", "warning", capsula1);
+
+};
