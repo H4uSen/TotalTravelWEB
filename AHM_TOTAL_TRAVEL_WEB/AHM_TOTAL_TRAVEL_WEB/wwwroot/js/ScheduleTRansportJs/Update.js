f447369db432c44e5117f813e6dd9ae8ad45b089
RonaldIsai <87435489+RonaldIsai@users.noreply.github.com>
2022-10-18 11:01:09 -0600

    caracola

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/ScheduleTRansportJs/Update.js
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/ScheduleTRansportJs/Update.js
@@ -6,6 +6,10 @@ $("#UpdateBottomSchedule").click(() => {
     $("#modalUpdate").modal('show');
 });
 
+$("#SendScheduleEdit").click(() => {
+    ValidarupdateSchedule();
+})
+
 $("#closeScheduleEdit").click(() => {
     $("#modalUpdate").modal('hide');
 });
@@ -17,10 +21,26 @@ function GetScheduleID(id) {
         $('#FechaViajeUpdate').val(response.data.fecha);
         $('#HoTr_HoraSalidaUpdate').val(response.data.hora_Salida);
         $('#HoTr_HoraLlegadaUpdate').val(response.data.hora_Llegada);
-        SetDropDownValue($("#DsTr_IDUpdate"), response.data.ciudad_Salida_ID);
 
         if ($("#ID_Horario").val != 0) {
+            SetDropDownValue($("#DsTr_IDUpdate"), response.data.iD_Destino);
             $("#modalUpdate").modal('show');
         }
+    }
+}
+
+function ValidarupdateSchedule() {
+
+    const ValidateArraySchedule = [
+        { validateMessage: "Seleccione un Destino", Jqueryinput: $("#DsTr_IDUpdate") },
+        { validateMessage: "Seleccione una Fecha", Jqueryinput: $("#FechaViajeUpdate") },
+        { validateMessage: "Rellene el Campo Hora Salida", Jqueryinput: $("#HoTr_HoraSalidaUpdate") },
+        { validateMessage: "Rellene el Campo Hora Llegada", Jqueryinput: $("#HoTr_HoraLlegadaUpdate") },
+    ];
+
+    const ScheduleValidateUpdate = ValidateForm(ValidateArraySchedule);
+
+    if (ScheduleValidateUpdate) {
+        $("#UpdateScheduleForm").submit();
     }
 }
