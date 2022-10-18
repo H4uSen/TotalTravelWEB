d8f545081b85d7a5f8696d3b4b38d09b719c8ec4
RonaldIsai <87435489+RonaldIsai@users.noreply.github.com>
2022-10-18 04:21:09 -0600

    ola

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/ScheduleTRansportJs/Update.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    $("#ID_Horario").hide();
+});
+
+$("#UpdateBottomSchedule").click(() => {
+    $("#modalUpdate").modal('show');
+});
+
+$("#closeScheduleEdit").click(() => {
+    $("#modalUpdate").modal('hide');
+});
+
+function GetScheduleID(id) {
+    var response = ajaxRequest("https://totaltravel.somee.com/API/ScheduleTransportation/Find?id=" + id);
+    if (response.code == 200) {
+        $('#ID_Horario').val(id);
+        $('#FechaViajeUpdate').val(response.data.fecha);
+        $('#HoTr_HoraSalidaUpdate').val(response.data.hora_Salida);
+        $('#HoTr_HoraLlegadaUpdate').val(response.data.hora_Llegada);
+        SetDropDownValue($("#DsTr_IDUpdate"), response.data.ciudad_Salida_ID);
+
+        if ($("#ID_Horario").val != 0) {
+            $("#modalUpdate").modal('show');
+        }
+    }
+}
