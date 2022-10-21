d13209d86d3c30891995770eb97bf2be10104cd3
BU3S0 <109097992+BU3S0@users.noreply.github.com>
2022-10-21 02:36:59 -0600

    Avances Pantalla paquetes predeterminados
    Co-Authored-By: Claudia1234567890 <109091065+Claudia1234567890@users.noreply.github.com>

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/BuyDefaults/Create.js
@@ -0,0 +1,13 @@
+﻿$("#DetailsBottom").click(() => {
+    $("#modalUpdate").modal('show');
+});
+
+function editar(id) {
+    var response = ajaxRequest("https://totaltravel.somee.com/API/DefaultPackages/Find?id=" + id);
+    if (response.code == 200) {
+        $("#id").val(id);
+        $("#Nombre").val(response.data.nombre);    
+        $("#modalUpdate").modal("show");
+    }
+}
+
