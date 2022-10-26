0714ff2f080a1bc996c89493253b2c111e08f354
Kiritsugu-sama <109097530+Kiritsugu-sama@users.noreply.github.com>
2022-10-26 08:29:10 -0600

    historial de compras al 5%

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/PayHistory/Information.js
@@ -0,0 +1,38 @@
+﻿
+
+    // inicialize code
+$("#reservasb").addClass("active");
+$('#Reservation_Details_Info .item').removeClass("active");
+$('#Reservation_Details_Info .item').addClass("disabled");
+ShowContent("Default_Item");
+
+// functions
+
+function ShowContent(content, index) {
+
+        var button = $("#Reservation_Details_Info .item")[index];
+if (!$(button).hasClass('disabled')) {
+    $('#Reservation_Details_Content .Reservation_Details_Content_Item').hide();
+$('#Reservation_Details_Content #' + content).show();
+$('#Reservation_Details_Info .item').removeClass("active");
+$(button).addClass("active");
+        }
+    }
+
+function ViewReservation(hoteid) {
+        var response = ajaxRequest("https://totaltravel.somee.com/API/Hotels/Find?id="+hoteid);
+
+        if (response.code == 200) {
+            var imagen = $('#imagen').val();
+            var hotels = response.data;
+            var imagenes = hotels.image_URL.split(',');
+            $('#imagen').attr("src", imagenes[0]);
+            $('#hotename').html(hotels.hotel);
+        }
+
+    $('#Reservation_Details_Info .item').removeClass("disabled");
+    ShowContent("frmReservation_Info", 0);
+
+}
+
+
