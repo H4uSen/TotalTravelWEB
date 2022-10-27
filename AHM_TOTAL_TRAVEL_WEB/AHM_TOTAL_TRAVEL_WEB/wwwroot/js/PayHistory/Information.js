b4da338c292d3dfb9d89583fcd90d2450e4874c0
Kiritsugu-sama <109097530+Kiritsugu-sama@users.noreply.github.com>
2022-10-26 21:15:36 -0600

    historial de pago al 75%

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/PayHistory/Information.js
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/PayHistory/Information.js
@@ -1,4 +1,8 @@
-﻿
+﻿var ReservacionActView = ajaxRequest("https://totaltravel.somee.com/API/ReservationActivitiesExtra/List");
+var response2 = ajaxRequest("https://totaltravel.somee.com/API/ReservationTransportation/List");
+var ReservacionDetView = ajaxRequest("https://totaltravel.somee.com/API/ReservationDetails/List");
+var ReservacionActHotView = ajaxRequest("https://totaltravel.somee.com/API/ReservationActivitiesHotels/List");
+
 
     // inicialize code
 $("#reservasb").addClass("active");
@@ -30,7 +34,7 @@ function ViewReservation(hoteid,transid) {
             $('#hotename').html(hotels.hotel);
         }
         
-        var response2 = ajaxRequest("https://totaltravel.somee.com/API/ReservationTransportation/List");
+        
 
         if (response2.code == 200) {
             var transp = response2.data;
@@ -51,12 +55,108 @@ function ViewReservation(hoteid,transid) {
                              
             }
         }
-        
-        
+        var ReservacionView = ajaxRequest("https://totaltravel.somee.com/API/Reservation/Find?id=" + transid);
 
+        if (ReservacionView.code == 200) {
+
+            var resv = ReservacionView.data;
+            var reshotid = resv.reservacionHotelID;
+            
+
+            if (ReservacionDetView.code == 200) {
+
+                var resv = ReservacionDetView.data;
+                var rooms = resv.filter(resva => resva.reservacionHotelID = reshotid);
+                $('#rooms').empty();
+                for (var i=0; i < rooms.length; i++){
+                    const item = rooms[i];
+                    var RoomFind = ajaxRequest("https://totaltravel.somee.com/API/Rooms/Find?id=" + item.habitacionID);
+                    var room = RoomFind.data;
+                    var imagenes = room.imageUrl.split(',');
+                    var wifi, balcon;
+                    if (room.wifi = true) {wifi = "si"; }
+                    else { wifi = "no"; }
+                    if (room.balcon == 1) { balcon = "si"; }
+                    else { balcon = "no";}
+                    divroom =
+                    `<div class="item">
+                        <div class="image">
+                            <img
+                                src="${imagenes[0]}">
+                        </div>
+                        <div class="content">
+                            <a class="header">${item.nombre_Habitacion} <div class="ui large label">${item.categoria_Habitacion}
+                            </div></a>
+                            <div class="meta">
+                                <span class="cinema">
+                                    ${room.descripcion}
+                                </span>
+                            </div>
+                            <div class="extra">
+                                <div class="ui label">Wifi: ${wifi}</div>
+                                <div class="ui label">Balcon: ${balcon}</div>
+                                <div class="ui label">Camas ${room.camas}</div>
+                            </div><br>
+                        </div>
+                        <div class="content left floated" style="text-align: end;">
+                            <a class="ui huge green tag label">${room.precio}</a>
+                        </div>
+                     </div>`                   
+                    $('#rooms').append(divroom);
+                }            
+            }
+            
+        }
+                
+       
+
+        if (ReservacionActView.code == 200) {
+
+            var resvact = ReservacionActView.data;
+            var activities = resvact.filter(resva => resva.reservacion = transid);
+            $('#actext').empty();
+            for (var i = 0; i < activities.length; i++) {
+                const item = activities[i];
+                var ActivitieFind = ajaxRequest("https://totaltravel.somee.com/API/Activities/Find?id=" + item.id_Actividad_Extra);
+                var Activitie = ActivitieFind.data;               
+                actext =
+                    `<div class="item">
+                        <div class="content">
+                            <a class="header">${item.actividad_Extra}</a>
+                            <div class="description">
+                                Tipo de actividad: ${Activitie.tipo}
+                            </div>                         
+                        </div>
+                    </div>`
+                $('#actext').append(actext);
+            }
+        }
+        if (ReservacionActHotView.code == 200) {
+
+            var resvact = ReservacionActHotView.data;
+            var activitiesh = resvact.filter(resva => resva.reservacion = transid);
+            $('#acthot').empty();
+            for (var i = 0; i < activitiesh.length; i++) {
+                const item = activitiesh[i];                            
+                
+                actext =
+                    `<div class="item">
+                            <div class="content">
+                                <a class="header">${item.nombre}</a>
+                                <div class="description">
+                                    Descripción: ${item.descripcion}
+                                </div>                         
+                            </div>
+                        </div>`
+                $('#acthot').append(actext);
+            }
+        }
+
+      
     $('#Reservation_Details_Info .item').removeClass("disabled");
     ShowContent("frmReservation_Info", 0);
 
 }
+
 
 
