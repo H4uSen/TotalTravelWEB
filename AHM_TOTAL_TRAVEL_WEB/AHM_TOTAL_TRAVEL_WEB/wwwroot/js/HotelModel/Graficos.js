123ea2913f868615b3a1e3ee4029e2f3d3c5f8c6
Kiritsugu-sama <109097530+Kiritsugu-sama@users.noreply.github.com>
2022-11-03 02:37:11 -0600

    Tílde, sigan viendo
    Co-Authored-By: Rodro08 <109097619+Rodro08@users.noreply.github.com>

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/HotelModel/Graficos.js
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/HotelModel/Graficos.js
@@ -32,7 +32,7 @@ function GraficaPastel() {
             type: 'pie'
         },
         title: {
-            text: 'Browser market shares in March, 2022'
+            text: 'Hoteles más reservados'
         },
         tooltip: {
             pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
