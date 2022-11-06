4df4b153896a2a28f99c350dca96b48c37cebafc
Werundy <angeljmacea@gmail.com>
2022-11-06 14:36:59 -0600

    Rol

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/RolesJs/Create.js
@@ -0,0 +1,44 @@
+﻿var send = false;
+$("#createRoles").click(() => {
+    $("#modalCreate").modal('show');
+});
+
+$("#closeRoles").click(() => {
+    $("#modalCreate").modal('hide');
+});
+
+$("#sendRoles").click(() => {
+
+    validateArrayForm = [
+        { validateMessage: "Ingrese un rol.", Jqueryinput: $("#Role_Descripcion") },
+    ];
+
+    // retorna bool 
+    const ValidateFormStatus = ValidateForm(validateArrayForm);
+    if (ValidateFormStatus) {
+        $('#createRolesForm').submit()
+    }
+
+});
+
+
+// ----------------------------------- EVENTS ------------------------------------
+$("#txtSearch").keyup(function () {
+    _this = this;
+    $.each($("#grdTypeMenus tbody tr"), function () {
+        if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1) {
+            $(this).hide();
+        }
+        else {
+            $(this).show();
+        }
+    });
+
+    $("#grdRoles").paginationTdA({
+        elemPerPage: 10
+    });
+});
+
+$("#grdRoles").paginationTdA({
+    elemPerPage: 10
+});
