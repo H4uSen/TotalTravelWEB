5ea227c807115c30d544016314ae37abaf4e29c9
Andres Leiva <andresleivaamaya@gmail.com>
2022-11-08 09:04:49 -0600

    la verdad ya me rendi, no les voy a mentir

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/ReservationController.cs
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/ReservationController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
 
 namespace AHM_TOTAL_TRAVEL_WEB.Controllers
@@ -15,15 +16,17 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
         private readonly HotelsService _hotelsService;
         private readonly AccessService _accessService;
         private readonly SaleServices _saleServices;
+        private readonly ActivitiesServices _activitiesServices;
 
-        public ReservationController(ReservationService reservationService, HotelsService hotelsService, AccessService accessService,
-            SaleServices saleServices)
+        public ReservationController(ReservationService reservationService, HotelsService hotelsService, AccessService accessService, SaleServices saleServices, ActivitiesServices activitiesServices)
         {
             _reservationService = reservationService;
             _hotelsService = hotelsService;
             _accessService = accessService;
             _saleServices = saleServices;
+            _activitiesServices = activitiesServices;
         }
+
         [HttpGet]
         public async Task<IActionResult> Index()
         {
@@ -68,10 +71,25 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
         public async Task<IActionResult> Update(int id)
         {
             string token = HttpContext.User.FindFirst("Token").Value;
+
+            
+
             var item = new ReservationViewModel();
             var list = await _reservationService.ReservationList(token);
             IEnumerable<ReservationListViewModel> data = (IEnumerable<ReservationListViewModel>)list.Data;
             var element = data.Where(x => x.ID == id).ToList()[0];
+
+            var responseHotelsActivities = await _hotelsService.HotelsActivitiesList(token);
+            List<HotelsActivitiesListViewModel> hotelsActivities = (List<HotelsActivitiesListViewModel>)responseHotelsActivities.Data;
+            hotelsActivities = hotelsActivities.Where(x => x.ID_Hotel == element.Hotel_ID).ToList();
+            ViewBag.hotelActivities = new MultiSelectList(hotelsActivities, "ID", "ddlItem");
+
+            HotelListViewModel hotel = (HotelListViewModel)_hotelsService.HotelFind(element.Hotel_ID.Value.ToString(), token).Result.Data;
+
+            var responseExtraActivities = await _activitiesServices.ExtraActivitiesList(token);
+            List<ActivitiesExtrasListViewModel> extraActivities = (List<ActivitiesExtrasListViewModel>)responseExtraActivities.Data;
+            extraActivities = extraActivities.Where(x => x.CiudadID == hotel.CiudadID).ToList();
+            ViewBag.extraActivities = new MultiSelectList(extraActivities, "ID", "ddlItem");
 
 
             var usuario = await _accessService.UsersList(token);
@@ -89,20 +107,48 @@ namespace AHM_TOTAL_TRAVEL_WEB.Controllers
 
             ViewData["Reservacion_HotelID"] = element.ReservacionHotelID;
             ViewData["Hotel_ID"] = element.Hotel_ID;
+            ViewData["Usua_ID"] = id;
+            ViewData["Reservation"] = element;
 
             return View(element);
         }
+
+        
         [HttpPost]
         public async Task<IActionResult> Update(ReservationViewModel reserva)
         {
-            if (ModelState.IsValid)
+            try
             {
-                return RedirectToAction("Index");
+                if (ModelState.IsValid)
+                {
+                    var token = HttpContext.User.FindFirst("Token").Value;
+                    string UserID = HttpContext.User.FindFirst("User_Id").Value;
+                    reserva.Resv_UsuarioCreacion = int.Parse(UserID);
+                    var result = await _reservationService.ReservationUpdate(reserva,reserva.Resv_ID, token);
+                    if (result.Success)
+                    {
+                        return View();
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Message);
+                    }
+
+
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View(reserva);
+                }
             }
-            else
+            catch (Exception)
             {
-                return View();
+
+                return RedirectToAction("Error", "Home");
             }
+            
         }
 
 
--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Models/HotelsActivitiesViewModel.cs
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Models/HotelsActivitiesViewModel.cs
@@ -8,16 +8,27 @@ namespace AHM_TOTAL_TRAVEL_WEB.Models
     public class HotelsActivitiesListViewModel
     {
         public int ID { get; set; }
+        public int ID_Actividad { get; set; }
         public string Actividad { get; set; }
         public string Descripcion { get; set; }
         public decimal? Precio { get; set; }
+        public int ID_Hotel { get; set; }
         public string HotelNombre { get; set; }
+        public string HotelDescripcion { get; set; }
+        public int? PartnerID { get; set; }
+        public string Image_URL { get; set; }
+        public int ID_UsuaCrea { get; set; }
         public string UsuarioCreacion { get; set; }
         public DateTime? FechaCreacion { get; set; }
+        public int? ID_UsuaModifica { get; set; }
         public string UsuarioModifica { get; set; }
         public DateTime? FechaModifica { get; set; }
         public bool? Estado { get; set; }
-        public int PartnerID { get; set; }
+
+        public string ddlItem => $"{Actividad} - L.{Precio} ";
+
+
+
 
     }
     public class HotelsActivitiesViewModel
