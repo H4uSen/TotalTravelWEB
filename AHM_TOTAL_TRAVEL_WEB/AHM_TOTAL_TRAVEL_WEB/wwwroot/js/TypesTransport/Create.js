cbbc56128deb423487e161690ca4a2a7aae00a5a
BU3S0 <109097992+BU3S0@users.noreply.github.com>
2022-11-09 09:50:06 -0600

    arreglos

--- AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/TypesTransport/Create.js
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/wwwroot/js/TypesTransport/Create.js
@@ -26,3 +26,35 @@ function validar() {
     }
 
 }
+
+function editar(id) {
+    var response = ajaxRequest("https://totaltravelapi.azurewebsites.net/API/TypesTransport/Find?id=" + id);
+    if (response.code == 200) {
+        $("#id").val(id);
+        $("#Transporte_up").val(response.data.trasporte);
+        $("#modalUpdate").modal("show");
+    }
+}
+function actualizar() {
+    validateArrayForm = [
+        { validateMessage: "Ingrese la descripcion.", Jqueryinput: $("#modalUpdate #Transporte_up") },
+    ];
+    const ValidateFormStatus = ValidateForm(validateArrayForm);
+    var id = $("#id").val();
+    if (ValidateFormStatus) {
+        var data = new FormData();
+        data.append("TiTr_Descripcion", $("#modalUpdate #Transporte_up").val());
+        data.append("TiTr_UsuarioModifica", Client_User_ID);
+
+       
+        var status = uploadFile(`https://totaltravelapi.azurewebsites.net/API/TypesTransport/Update?id=${id}`, data, "PUT");
+        if (status.code == 200) {
+            location.reload();
+            if (status.data.codeStatus > 0) {
+                window.location.href = '/TypesTransport?success=true';
+            }
+        }
+
+    }
+
+}
