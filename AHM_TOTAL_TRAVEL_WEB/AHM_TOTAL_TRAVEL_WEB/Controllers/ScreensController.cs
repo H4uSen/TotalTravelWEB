8b305b9f71ce5c75ec62bb8556d9300c4b53e02d
didier bonilla <didierbonilla25@gmail.com>
2022-11-19 09:49:53 -0600

    INDEX SCREENS CRUD

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_WEB/Controllers/ScreensController.cs
@@ -0,0 +1,30 @@
+﻿using AHM_TOTAL_TRAVEL_WEB.Models;
+using AHM_TOTAL_TRAVEL_WEB.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_WEB.Controllers
+{
+    public class ScreensController : Controller
+    {
+        private readonly GeneralService _GeneralServices;
+        private readonly AccessService _AccessService;
+        public ScreensController(GeneralService GeneralService, AccessService AccessService)
+        {
+            _GeneralServices = GeneralService;
+            _AccessService = AccessService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            string token = HttpContext.User.FindFirst("Token").Value;
+            var screensList = (IEnumerable<PermissionsViewModel>)(await _AccessService.PermissionsList(token)).Data;
+            return View(screensList);
+        }
+    }
+}
