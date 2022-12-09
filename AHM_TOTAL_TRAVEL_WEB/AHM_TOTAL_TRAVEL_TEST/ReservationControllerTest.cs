c2f211e8c6bdc0dfd851da0fa383490f1b6fef04
Andres Leiva <andresleivaamaya@gmail.com>
2022-12-09 08:27:47 -0600

    Proyecto para los tests

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_TEST/ReservationControllerTest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AHM_TOTAL_TRAVEL_TEST
+{
+    internal class ReservationControllerTest
+    {
+    }
+}
---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_TEST/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace AHM_TOTAL_TRAVEL_TEST
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
