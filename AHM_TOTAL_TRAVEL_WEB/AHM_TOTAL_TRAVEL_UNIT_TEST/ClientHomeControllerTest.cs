57a406eb480739b1780188a655b09a15e38f4687
Werundy <angeljmacea@gmail.com>
2022-12-11 08:40:13 -0600

    UnitTests DefaultPackages, ClientHome

---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_UNIT_TEST/ClientHomeControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using AHM_TOTAL_TRAVEL_WEB.Controllers;
+using AHM_TOTAL_TRAVEL_WEB.Services;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHM_TOTAL_TRAVEL_UNIT_TEST
+{
+    public class ClientHomeControllerTest
+    {
+        SaleServices _saleServices;
+
+        [Test]
+        public void TestIndex()
+        {
+            // Arrange
+            ClientHomeController controller = new ClientHomeController(_saleServices);
+
+            // Act
+            var result = controller.Index().Result;
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+    }
+}
---
+++ AHM_TOTAL_TRAVEL_WEB/AHM_TOTAL_TRAVEL_UNIT_TEST/DefaultPackagesControllerTest.cs
@@ -0,0 +1,93 @@
+﻿using AHM_TOTAL_TRAVEL_WEB.Controllers;
+using AHM_TOTAL_TRAVEL_WEB.Models;
+using AHM_TOTAL_TRAVEL_WEB.Services;
+using Microsoft.AspNetCore.Mvc;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AHM_TOTAL_TRAVEL_UNIT_TEST
+{
+    [TestFixture]
+    public class DefaultPackagesControllerTest
+    {
+        SaleServices _saleServices;
+        HotelsService _HotelsService;
+        RestaurantService _RestaurantService;
+
+        [Test]
+        public void TestIndex()
+        {
+            // Arrange
+            DefaultPackagesController controller = new DefaultPackagesController(
+                _saleServices,
+                _HotelsService,
+                _RestaurantService
+                );
+
+            // Act
+            var result = controller.Index().Result;
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+
+
+        [Test]
+        public void TestCreate()
+        {
+            // Arrange
+            DefaultPackagesController controller = new DefaultPackagesController(
+               _saleServices,
+               _HotelsService,
+               _RestaurantService
+               );
+
+            // Act
+            var result = controller.Create().Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+
+        [Test]
+        public void TestUpdate()
+        {
+            // Arrange
+            DefaultPackagesController controller = new DefaultPackagesController(
+                _saleServices,
+                _HotelsService,
+                _RestaurantService
+                );
+
+            // Act
+            var result = controller.Update(2).Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+        [Test]
+        public void TestDelete()
+        {
+            // Arrange
+            DefaultPackagesController controller = new DefaultPackagesController(
+                 _saleServices,
+                 _HotelsService,
+                 _RestaurantService
+                 );
+
+
+            // Act
+            DefaultPackagesViewModel model = new DefaultPackagesViewModel();
+            model.paqu_UsuarioModifica = 2;
+            var result = controller.Delete(model, 0).Result;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOf<IActionResult>(result);
+        }
+    }
+}
